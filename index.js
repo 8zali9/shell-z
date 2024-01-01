5828dc050bc19f7e4415553e867c6b0ada288a63
Zulfiqar Ali <8zali9@gmail.com>
2024-01-01 21:52:15 +0500

    commands finalized.

--- index.js
+++ index.js
@@ -113,34 +113,3 @@ function getRunningProcesses() {
     console.log(processes.join("\n"));
   });
 }
-
-// ------------------------------------------------------------------------------------------------------------
-
-// @desc:  changing path
-//                                         !!! NOT COMPLETED: CANNOT CHANGE THE PATH
-function changePathFromCurrentDirectory() {
-  const jottedDirName = process.argv[2];
-  const currPath = path.join(process.cwd(), jottedDirName);
-  console.log(currPath);
-  console.log(jottedDirName);
-  process.chdir(currPath);
-}
-
-// @desc:  wiping/cleaning the screen
-function wipeScreen() {
-  exec("cls", (error, stdout, stderr) => {
-    if (error) {
-      console.log(error);
-    }
-  });
-}
-
-function promptChange() {
-  exec("prompt shell-z ~ ", (error, stdout, stderr) => {
-    if (error) {
-      console.log(error);
-    }
-  });
-}
-
-promptChange();
--- raw.js
+++ raw.js
@@ -18,3 +18,34 @@ function writeToFile() {
     });
   });
 }
+
+// ------------------------------------------------------------------------------------------------------------
+
+// @desc:  changing path
+//                                         !!! NOT COMPLETED: CANNOT CHANGE THE PATH
+function changePathFromCurrentDirectory() {
+  const jottedDirName = process.argv[2];
+  const currPath = path.join(process.cwd(), jottedDirName);
+  console.log(currPath);
+  console.log(jottedDirName);
+  process.chdir(currPath);
+}
+
+// @desc:  wiping/cleaning the screen
+function wipeScreen() {
+  exec("cls", (error, stdout, stderr) => {
+    if (error) {
+      console.log(error);
+    }
+  });
+}
+
+function promptChange() {
+  exec("prompt shell-z ~ ", (error, stdout, stderr) => {
+    if (error) {
+      console.log(error);
+    }
+  });
+}
+
+promptChange();
